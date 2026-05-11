aeb9266d1462e3c38c9055741cc79a37b16bf37d
CupRuska <noname@email.com>
2026-05-11 16:45:50 +0300

    Base main func in sdl

--- main.c
+++ main.c
@@ -1,7 +1,30 @@
 #include <stdio.h>
+#include <stdbool.h>
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
+#include <SDL3/SDL_error.h>
+
+bool runningLoop = 1;
 
 int main(void) {
-    printf("%s", "Hello world\n");
+    printf("%s", "Load a Program\n");
+    if(!SDL_Init(SDL_INIT_VIDEO)) { // if init is !true SDL_GetError
+        printf("Error with init window %s\n", SDL_GetError());
+    }
+
+    SDL_Window* window = SDL_CreateWindow("Hello world", 800, 600, SDL_WINDOW_MINIMIZED);
+    
+    if(window == NULL) {
+        fprintf(stderr, "Error with create window: %s\n", SDL_GetError());
+        SDL_Quit();
+        return 1; 
+    }
+
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, 0);
+
+    while(runningLoop) {
+        break;
+    }
     
     return 0;
 }
--- makefile
+++ makefile
@@ -2,7 +2,7 @@ CC = clang
 MAIN_FILE = main.c
 OUTPUTFILE = binary/main
 
-CCCommand = $(MAIN_FILE) -o $(OUTPUTFILE)
+CCCommand = $(MAIN_FILE) -o $(OUTPUTFILE) `pkg-config --cflags --libs sdl3`
 
 build:
 	$(CC) $(CCCommand)
